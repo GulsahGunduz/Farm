07aeeb35550dd2832fad7483dc62f421ef5bcb6c
GulsahGunduz <76705491+GulsahGunduz@users.noreply.github.com>
2023-07-31 06:41:16 +0300

    Plant

--- Land/Assets/Scripts/PlantController.cs
+++ Land/Assets/Scripts/PlantController.cs
@@ -1,14 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlantController : MonoBehaviour
 {
-    
-    private void OnTriggerEnter(Collider other) 
+    [SerializeField] float growUpTime;
+    Vector3 originalScale;
+    bool isReadyToPick;
+
+    void Start()
     {
-        
+        isReadyToPick = true;
+        originalScale = transform.localScale;
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && isReadyToPick)
+        {
+            StartCoroutine(ProductPicked());
+        }
+    }
 
+    IEnumerator ProductPicked()
+    {
+        isReadyToPick = false;
+        Vector3 targetScale = originalScale / 2.5f;
+        transform.DOScale(targetScale, 1f);
+
+        yield return new WaitForSeconds(growUpTime);
+
+        transform.DOScale(originalScale, 5f).SetEase(Ease.InOutBack);
+        isReadyToPick = true;
+    }
 }
