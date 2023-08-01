77cb167a853efb5842615e4d96c2c61826bf485e
GulsahGunduz <76705491+GulsahGunduz@users.noreply.github.com>
2023-08-01 05:10:34 +0300

    UIManager

---
+++ Land/Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    [SerializeField] TextMeshProUGUI coinText;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(instance);
+        }
+    }
+
+    public void ShowCoinCountOnScreen(int coins)
+    {
+        coinText.text = coins.ToString();
+    }
+}
